b4a5fef4d0aaa08fd16f9b4dcffe686d3e201d52
Puteulanus <mj@puteulanus.com>
2016-02-29 01:23:55 +0800

    更新apt源

--- script/source-update.sh
+++ script/source-update.sh
@@ -6,9 +6,12 @@ deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe mult
 deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-updates main restricted universe multiverse
 deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-backports main restricted universe multiverse
 deb mirror://mirrors.ubuntu.com/mirrors.txt trusty-security main restricted universe multiverse
+deb http://cz.archive.ubuntu.com/ubuntu trusty main
 _EOF_
 cp -f /etc/apt/sources.list /etc/apt/sources.list.save
 mv /tmp/script/apt/sources-list/* /etc/apt/sources.list.d/
+
+add-apt-repository ppa:ubuntu-toolchain-r/test
 
 apt-get update
 
